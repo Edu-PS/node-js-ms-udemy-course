057b0aa5e3faf9c8f5b4cc511180f8c28356c476
Luis Eduardo Peraza Salazar <le.perazasalazar@gmail.com>
2023-07-24 20:13:01 +0200

    Completado el reference-variables

--- Section-02/Code/reference-variables.js
+++ Section-02/Code/reference-variables.js
@@ -13,3 +13,16 @@ let obj1 = {
     surname: "Perez",
     age: 24
 }
+
+// Realiza una copia completa elemento a elemento, no realiza puntero a la ubicacion de arr1.
+let arr2 = arr1.slice()
+// Se guarda un puntero a la ubicacion del arr1.
+let arr3 = arr1
+// Cambio del valor original
+arr1[0] = 20
+
+console.log()
+console.log('Valor del Array original arr1: ', arr1)
+console.log('Valor del Array copiado con Slice() antes de modificar arr1[0]: ', arr2)
+console.log('Valor del Array utilizando el signo igual(=) y copiado antes de modificar arr1[0] :', arr3)
+console.log()
