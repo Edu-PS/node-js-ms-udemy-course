25148a2959b3d4d8d686728380b0f5cf4400128f
Luis Eduardo Peraza Salazar <le.perazasalazar@gmail.com>
2023-07-26 16:03:47 +0200

    Secc-03 basic-server.js

---
+++ Section-03/Code/basic-server.js
@@ -0,0 +1,18 @@
+ // IMPORTANDO LA LIBRERIA http DEL ENTORNO GLOBAL
+// import http from 'http'; // module
+const http = require('http'); // CommonJS
+
+// Creando la funcion que se ejecutara con cada request (DOS MANERAS DIFERENTES)
+// function reqFunction (request, response) {
+//     console.log(request);
+// }
+const reqFunction = (request, response) => {
+    console.log(request);
+}
+
+// CREANDO EL SERVIDOR y proporcionando la funcion que se ejecutara cada vez que se reciba una solicitud
+const server = http.createServer(reqFunction);
+
+// DEJANDO ATENTO AL SERVIDOR A CUALQUIER REQUEST - INICIA EL SERVIDOR
+// -> si se va al navegador y se ejecuta http://localhost:3000/ se vera la respuesta por el console.log(req)
+server.listen(3000);
